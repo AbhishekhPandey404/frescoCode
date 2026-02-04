cfc98790838de7b57554a67d29583d2b8ba3355e
labuser <labuser@172.31.42.69>
2026-02-04 04:24:45 +0000

    hi

---
+++ MavenTestNgEx/src/main/java/windows/excel_ex.java
@@ -0,0 +1,5 @@
+package windows;
+
+public class excel_ex {
+
+}
---
+++ MavenTestNgEx/src/test/java/stepdefination/testrunner.java
@@ -0,0 +1,20 @@
+package stepdefination;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@SuppressWarnings("deprecation")
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src/test/resources/feature",
+		glue = ("stepdefination"),
+		monochrome = true,
+		plugin = {"pretty","html:target/HtmlReports.html","json:target/JSONReports.xml"})
+		
+public class testrunner extends AbstractTestNGCucumberTests{
+	
+
+}
